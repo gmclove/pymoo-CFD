e68597f81744b79f1bcbb9bf83e57a7ba3832e72
George Love <george.love077@gmail.com>
2021-01-27 13:20:49 -0500

    change python jobslurm for no_geo case

--- YALES2/cases/ics_2D_cylinder-no_geo/jobslurm.sh
+++ YALES2/cases/ics_2D_cylinder-no_geo/jobslurm.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-#SBATCH --partition=ib --constraint="ib&haswell_1"
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --time=3:00:00
 #SBATCH --mem-per-cpu=2G
-#SBATCH --job-name=1j
 #SBATCH --output=output.dat
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=glove1@uvm.edu
